146397d0e77f4892fc0809af471e595f0c7e43aa
orthsi <simon.orth@gmx.de>
2023-06-17 14:06:26 +0200

    linting setup done, added card component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,8 @@
-import { TestBed } from "@angular/core/testing";
-import { RouterTestingModule } from "@angular/router/testing";
-import { AppComponent } from "./app.component";
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
 
-describe("AppComponent", () => {
+describe('AppComponent', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
@@ -10,24 +10,24 @@ describe("AppComponent", () => {
     })
   );
 
-  it("should create the app", () => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it("should have as title 'vocabulary'", () => {
+  it(`should have as title 'vocabulary'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual("vocabulary");
+    expect(app.title).toEqual('vocabulary');
   });
 
-  it("should render title", () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector(".content span")?.textContent).toContain(
-      "vocabulary app is running!"
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'vocabulary app is running!'
     );
   });
 });
---
+++ src/app/word-card/word-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-word-card',
+  templateUrl: './word-card.component.html',
+  styleUrls: ['./word-card.component.css']
+})
+export class WordCardComponent {
+
+}
