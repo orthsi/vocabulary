3745e8c4923839af7ce8fb087f0a0df61f817790
orthsi <simon.orth@gmx.de>
2023-06-17 15:36:41 +0200

    added nav bar and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LearningPageComponent } from './pages/learning-page/learning-page.component';
+import { StartPageComponent } from './pages/start-page/start-page.component';
 
 const routes: Routes = [
   { path: 'learn', component: LearningPageComponent },
+  { path: '', component: StartPageComponent },
   // other routes...
 ];
 
